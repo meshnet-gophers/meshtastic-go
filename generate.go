212cf4831ca61c57e07df5635f41ea75354cdbea
Smoke <86024507+crypto-smoke@users.noreply.github.com>
2024-02-29 12:54:07 -1000

    update protobufs and build vtproto with less options

--- generate.go
+++ generate.go
@@ -1,2 +1,2 @@
-//go:generate protoc  -I protobufs\ --go_out=. --go_opt=paths=source_relative --go-vtproto_out=paths=source_relative:. protobufs\meshtastic\*.proto
+//go:generate protoc  -I ./protobufs/ --go_out=. --go_opt=paths=source_relative --go-vtproto_out=paths=source_relative:. --go-vtproto_opt=features=marshal+unmarshal+size ./protobufs/meshtastic/*.proto
 package meshtastic
